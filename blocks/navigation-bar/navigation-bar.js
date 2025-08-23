ec6999c2d7d16737a7c9ca4af1aba5004db721e5
Gopichand0877 <gopichand.reddy.pulagam@ibm.com>
2025-08-24 03:13:12 +0530

    v3

--- blocks/navigation-bar/navigation-bar.js
+++ blocks/navigation-bar/navigation-bar.js
@@ -4,7 +4,7 @@
 
 const NAV_CONFIG = {
   brand: {
-    logo: "/icons/cp-logo.svg", // ← change to your logo URL
+    logo: "/icons/cp_logo.png" // ← change to your logo URL
     alt: "Chicago Pneumatic",
     href: "/",
   },
